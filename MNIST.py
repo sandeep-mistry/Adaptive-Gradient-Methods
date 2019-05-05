98f74c286d66cc94672803ec944749b4d5ce0bab
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-05 20:20:24 +0100

    Update MNIST.py

--- MNIST.py
+++ MNIST.py
@@ -20,8 +20,8 @@ def get_parser():
                         choices=['resnet', 'densenet', 'Simple_MLP','MLP_Dropout','SLP_model'])
     parser.add_argument('--optim', default='sgd', type=str, help='optimizer',
                         choices=['sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'])
-    parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--final_lr', default=0.001, type=float,
+    parser.add_argument('--lr', default=100, type=float, help='learning rate')
+    parser.add_argument('--final_lr', default=0.01, type=float,
                         help='final learning rate of AdaBound')
     parser.add_argument('--gamma', default=1e-3, type=float,
                         help='convergence speed term of AdaBound')
@@ -191,7 +191,7 @@ else:
 net = build_model(args, device, ckpt=ckpt)
 criterion = nn.CrossEntropyLoss()
 optimizer = create_optimizer(args, net.parameters())
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=75, gamma=0.1,
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1,
                                           last_epoch=start_epoch)
 
 train_accuracies = []
