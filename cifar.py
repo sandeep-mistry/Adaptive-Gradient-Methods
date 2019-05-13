db7e98f4339ba14c863df256ac9e7b35a6c9f154
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-13 17:34:13 +0100

    Update cifar.py

--- cifar.py
+++ cifar.py
@@ -19,8 +19,8 @@ def get_parser():
                         choices=['resnet', 'densenet'])
     parser.add_argument('--optim', default='adabound', type=str, help='optimizer',
                         choices=['sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'])
-    parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--final_lr', default=0.0001, type=float,
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--final_lr', default=0.01, type=float,
                         help='final learning rate of AdaBound')
     parser.add_argument('--gamma', default=0.1, type=float,
                         help='convergence speed term of AdaBound')
@@ -59,7 +59,7 @@ def build_dataset():
     return train_loader, test_loader
 
 
-def get_ckpt_name(model='resnet', optimizer='adabound', lr=0.001, final_lr=0.0001, momentum=0.9,
+def get_ckpt_name(model='resnet', optimizer='adabound', lr=0.1, final_lr=0.01, momentum=0.9,
                   beta1=0.9, beta2=0.999, gamma=0.1):
     name = {
         'sgd': 'lr{}-momentum{}'.format(lr, momentum),
