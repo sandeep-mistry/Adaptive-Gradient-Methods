329c042e1b3de14b2f5923977b9156abe81a9dd3
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-14 23:23:58 +0100

    Create run_mnist.py

---
+++ run_mnist.py
@@ -0,0 +1,9 @@
+import MNIST_adam
+import MNIST_amsgrad
+import MNIST_adabound
+import MNIST_amsbound
+
+execfile('MNIST_adam.py')
+execfile('MNIST_amsgrad.py')
+execfile('MNIST_adabound.py')
+execfile('MNIST_amsbound.py')
