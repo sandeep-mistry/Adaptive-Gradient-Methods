a43e31c6cd4236f3262224914be4fa937b749463
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-14 23:28:27 +0100

    Update run_mnist.py

--- run_mnist.py
+++ run_mnist.py
@@ -3,7 +3,12 @@ import MNIST_amsgrad
 import MNIST_adabound
 import MNIST_amsbound
 
-execfile('MNIST_adam.py')
-execfile('MNIST_amsgrad.py')
-execfile('MNIST_adabound.py')
-execfile('MNIST_amsbound.py')
+# execfile('MNIST_adam.py')
+# execfile('MNIST_amsgrad.py')
+# execfile('MNIST_adabound.py')
+# execfile('MNIST_amsbound.py')
+
+exec(open('MNIST_adam.py').read())
+exec(open('MNIST_amsgrad.py').read())
+exec(open('MNIST_adabound.py').read())
+exec(open('MNIST_amsbound.py').read())
