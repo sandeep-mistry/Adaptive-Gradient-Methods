3063411930ccee36c731bcc98bb09a263160a63e
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-14 23:47:50 +0100

    Update run_mnist.py

--- run_mnist.py
+++ run_mnist.py
@@ -1,10 +1,4 @@
-
-
-# execfile('MNIST_adam.py')
-# execfile('MNIST_amsgrad.py')
-# execfile('MNIST_adabound.py')
-# execfile('MNIST_amsbound.py')
-
+exec(open('MNIST.py').read())
 exec(open('MNIST_adam.py').read())
 exec(open('MNIST_amsgrad.py').read())
 exec(open('MNIST_adabound.py').read())
