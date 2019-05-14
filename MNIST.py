8ac2e1daacc1b091516e165cf33f937ec0ab824c
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-15 00:02:10 +0100

    Update MNIST.py

--- MNIST.py
+++ MNIST.py
@@ -12,8 +12,8 @@ from models import *
 from adabound import AdaBound
 
 
-learning_rate = 0.05
-final_learning_rate = 0.05
+learning_rate = 10
+final_learning_rate = 10
 model_choice = 'SLP_model'  # 'resnet', 'densenet', 'SLP_Model'
 optim_choice = 'sgd'  # 'sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'
 momentum_choice = 0.9
