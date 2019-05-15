5e4391a1bcff22e3ebc069e0fef3b163e4fa6422
MichelleA94 <47279898+MichelleA94@users.noreply.github.com>
2019-05-15 15:25:17 +0100

    Update MNIST.py

--- MNIST.py
+++ MNIST.py
@@ -14,7 +14,7 @@ from adabound import AdaBound
 
 learning_rate = 0.05
 final_learning_rate = 0.05
-model_choice = 'Simple_MLP'  # 'resnet', 'densenet', 'SLP_Model'
+model_choice = 'SLP_Model'  # 'resnet', 'densenet', 'SLP_Model'
 optim_choice = 'sgd'  # 'sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'
 momentum_choice = 0.9
 beta_1 = 0.99
@@ -37,11 +37,11 @@ def build_dataset():
         transforms.Lambda(lambda x: x.view(-1))
     ])
 
-    trainset = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True,
+    trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True,
                                             transform=transform_train)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
 
-    testset = torchvision.datasets.FashionMNIST(root='./data', train=False, download=True,
+    testset = torchvision.datasets.MNIST(root='./data', train=False, download=True,
                                            transform=transform_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
 
