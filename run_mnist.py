7d4ef64cf6f525c033f2da64e3bd7ed6584bdda5
Sandeep Mistry <41234286+sandeep-mistry@users.noreply.github.com>
2019-05-17 13:35:35 +0100

    Delete run_mnist.py

--- run_mnist.py
+++
@@ -1,5 +0,0 @@
-exec(open('MNIST.py').read())
-exec(open('MNIST_adam.py').read())
-exec(open('MNIST_amsgrad.py').read())
-exec(open('MNIST_adabound.py').read())
-exec(open('MNIST_amsbound.py').read())
